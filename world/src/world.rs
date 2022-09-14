3713ce9760241cfb38e8b533cfa2d617244b907d
Numericly <107078148+numericly@users.noreply.github.com>
2022-09-13 22:09:35 -0400

    Broken changes

--- world/src/world.rs
+++ world/src/world.rs
@@ -3,6 +3,7 @@ use quartz_nbt::{
     NbtCompound, NbtList,
 };
 use std::{
+    convert::TryInto,
     fs,
     io::{Cursor, Read},
 };
@@ -59,13 +60,11 @@ pub struct Entity {
 }
 
 pub fn read_region_file(file_path: String) -> Vec<Chunk> {
+    let start = std::time::Instant::now();
     let chunk_binary = fs::read(file_path.clone()).unwrap();
-
-    //println!("Chunk binary: {:?}", &mut chunk_binary);
+    println!("Read file in {}ms", start.elapsed().as_secs_f32() * 1000.0);
 
     let mut cursor = Cursor::new(chunk_binary);
-
-    // println!("Cursor has {} lines", &cursor.lines().count());
 
     let mut chunk_positions = [0u8; 4096];
     cursor.read_exact(&mut chunk_positions).unwrap();
@@ -73,80 +72,118 @@ pub fn read_region_file(file_path: String) -> Vec<Chunk> {
     let mut chunk_timestamps = [0u8; 4096];
     cursor.read_exact(&mut chunk_timestamps).unwrap();
 
-    //println!("Chunk tables: {:x?}", buffer.len());
-
     let mut buffer2 = [0u8; 5];
-    let _chunk_header = cursor.read_exact(&mut buffer2);
+    let _chunk_header = cursor.read_exact(&mut buffer2).unwrap();
 
-    //println!("Chunk header: {:x?}", buffer2);
+    //println!("Chunk header: {:?}", buffer2);
 
-    let chunks = vec![];
+    println!("Cursor position: {}", cursor.position());
 
-    let mut been_accessed = [0u8; 2000];
+    let file_header_size = cursor.position();
 
-    //for i in 0..chunk_positions.len() / 4 {
+    //for i in (0..32 * 32).step_by(4) {
     for i in 0..1 {
-        // print!("{} ", chunk_positions[i*4] as u64 * 256 * 256 + chunk_positions[i*4+1] as u64 * 256 + chunk_positions[i*4+2] as u64);
-        let chunk_binary = fs::read(file_path.clone()).unwrap();
-        let cursor = Cursor::new(chunk_binary);
+        let bytes = [
+            chunk_positions[i + 2],
+            chunk_positions[i + 1],
+            chunk_positions[i],
+            0,
+        ];
+
+        let chunk_position = u32::from_le_bytes(bytes);
+
+        // println!(
+        //     "Position: {}",
+        //     file_header_size + (chunk_position * 4096) as u64,
+        // );
+
         read_chunk_nbt(
-            cursor,
-            8197 + 4096
-                * (chunk_positions[i * 4] as u64 * 256 * 256
-                    + chunk_positions[i * 4 + 1] as u64 * 256
-                    + chunk_positions[i * 4 + 2] as u64),
-        );
-        been_accessed[(chunk_positions[i * 4] as u64 * 256 * 256
-            + chunk_positions[i * 4 + 1] as u64 * 256
-            + chunk_positions[i * 4 + 2] as u64) as usize] = 1;
+            i,
+            &mut cursor,
+            file_header_size + (chunk_position * 4096) as u64,
+        )
     }
 
-    // println!("");
-
-    // println!("Been accessed: {:x?}", been_accessed);
-
-    //println!("Chunk positions: {:?}", chunk_positions);
-
-    chunks
+    vec![]
 }
 
-fn read_chunk_nbt(mut cursor: Cursor<Vec<u8>>, pointer: u64) {
+fn read_chunk_nbt(i: usize, cursor: &mut Cursor<Vec<u8>>, pointer: u64) {
     cursor.set_position(pointer);
-    let chunk_nbt = match io::read_nbt(&mut cursor, Flavor::ZlibCompressed) {
+
+    let chunk_nbt = match io::read_nbt(cursor, Flavor::ZlibCompressed) {
         //nbt data for the current chunk
         Ok(chunk_nbt) => chunk_nbt.0,
-        Err(e) => panic!("Error reading NBT: {}", e),
+        Err(e) => {
+            println!("Error reading NBT: {}, inx {}", e, i);
+            return;
+        }
     };
 
     let sections: &NbtList = chunk_nbt.get::<_, _>("sections").unwrap();
 
-    for i in 0..1 {
+    for i in 0..sections.len() {
         let section = sections.get::<&NbtCompound>(i).unwrap();
-        let block_states = section.get::<_, &NbtCompound>("block_states").unwrap();
-        let block_data = block_states.get::<_, &[i64]>("data").unwrap();
-        let palette = block_states.get::<_, &NbtList>("palette").unwrap();
 
-        const BITS_PER_BYTE: usize = 8;
+        println!("Section: {:?}", section);
+        // let section = sections.get::<&NbtCompound>(i).unwrap();
+        // let block_states = match section.get::<_, &NbtCompound>("block_states") {
+        //     Ok(block_data) => block_data,
+        //     Err(_) => {
+        //         //println!("Section {} is empty", section);
+        //         continue;
+        //     }
+        // };
+        // let block_data = match block_states.get::<_, &[i64]>("data") {
+        //     Ok(block_data) => block_data,
+        //     Err(_) => {
+        //         //println!("Section {} is empty", section);
+        //         continue;
+        //     }
+        // };
+        // let palette = block_states.get::<_, &NbtList>("palette").unwrap();
+        // let bit_array = BitArray::new(block_data, 4096);
 
-        let bits_per_block =
-            BITS_PER_BYTE * std::mem::size_of::<usize>() - (4usize.leading_zeros() as usize) - 1;
+        // let bits_per_block = 4;
 
-        println!("Bits per block: {}", bits_per_block);
-        println!("Palette: {:?}", palette);
+        // const CHUNK_WIDTH: usize = 16;
+        // const CHUNK_HEIGHT: usize = 16;
 
-        const CHUNK_WIDTH: usize = 16;
-        const CHUNK_HEIGHT: usize = 16;
+        // //println!("Bits per block: {}", bits_per_block);
+        // if palette.len() > 4 {
+        //     println!("Bit array: {:?}", bit_array);
+        //     println!("Palette: {:?}", palette);
+        // }
+    }
+}
 
-        let individual_value_mask = (1 << bits_per_block) - 1;
+#[derive(Debug)]
+struct BitArray<'a> {
+    data: &'a [i64],
+    bits_per_value: usize,
+    individual_value_mask: i64,
+}
 
-        for y in 0..CHUNK_HEIGHT {
-            for z in 0..CHUNK_WIDTH {
-                for x in 0..CHUNK_WIDTH {
-                    let block_number: usize =
-                        ((((y * CHUNK_HEIGHT) + z) * CHUNK_WIDTH) + x) as usize;
-                    let start_long = 64 / bits_per_block;
-                }
-            }
+impl BitArray<'_> {
+    fn new<'a>(data: &'a [i64], size: usize) -> BitArray {
+        let bits_per_value = ((data.len() * 64) + size - 1) / size;
+        let individual_value_mask = (1 << bits_per_value) - 1;
+        BitArray {
+            data,
+            bits_per_value,
+            individual_value_mask,
         }
+    }
+    fn get_3d(&self, x: usize, y: usize, z: usize) -> usize {
+        const SECTION_WIDTH: usize = 16;
+        const SECTION_HEIGHT: usize = 16;
+        let block_number: usize = ((((y * SECTION_HEIGHT) + z) * SECTION_WIDTH) + x) as usize;
+        self.get(block_number)
+    }
+    fn get(&self, index: usize) -> usize {
+        let values_per_long = 64 / self.bits_per_value;
+        let long_index = index / values_per_long;
+        let long_value = self.data[long_index];
+        let value_index = index % values_per_long;
+        ((long_value >> (value_index * self.bits_per_value)) & self.individual_value_mask) as usize
     }
 }
