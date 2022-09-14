26e1e3fbe0fd9ecd43bd2476fdc0a460b8f3187b
numericly <107078148+numericly@users.noreply.github.com>
2022-09-14 15:32:33 -0400

    Update world.rs

--- world/src/world.rs
+++ world/src/world.rs
@@ -3,63 +3,12 @@ use quartz_nbt::{
     NbtCompound, NbtList,
 };
 use std::{
-    convert::TryInto,
     fs,
     io::{Cursor, Read},
+    time::Instant,
 };
 
-pub struct Chunk {
-    pub x: i32,
-    pub z: i32,
-    pub sections: Vec<ChunkSection>,
-    pub biomes: Vec<i32>,
-    pub height_maps: Vec<i32>,
-    pub block_entities: Vec<BlockEntity>,
-    pub entities: Vec<Entity>,
-    pub block_light: Vec<i32>,
-    pub sky_light: Vec<i32>,
-}
-
-#[derive(Debug)]
-pub struct ChunkSection {
-    pub block_count: i16,
-    pub block_states: PalettedContainer,
-    pub biomes: PalettedContainer,
-}
-
-#[derive(Debug)]
-pub struct PalettedContainer {
-    pub bits_per_entry: u8,
-    //pub palette: todo(),
-    pub data_length: i32, //varint
-    pub data: Vec<i64>,
-}
-
-pub struct BlockEntity {
-    pub x: i32,
-    pub y: i32,
-    pub z: i32,
-    //pub nbt: todo(),
-}
-
-pub struct Entity {
-    pub uuid: u128,
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
-    pub yaw: f32,
-    pub pitch: f32,
-    pub head_pitch: f32,
-    pub velocity_x: f32,
-    pub velocity_y: f32,
-    pub velocity_z: f32,
-    pub on_ground: bool,
-    pub entity_id: i32,
-    pub entity_type: i32,
-    pub nbt: bool, // FIX THIS XAVIER
-}
-
-pub fn read_region_file(file_path: String) -> Vec<Chunk> {
+pub fn read_region_file(file_path: String) -> Vec<String> {
     let start = std::time::Instant::now();
     let chunk_binary = fs::read(file_path.clone()).unwrap();
     println!("Read file in {}ms", start.elapsed().as_secs_f32() * 1000.0);
@@ -98,7 +47,6 @@ pub fn read_region_file(file_path: String) -> Vec<Chunk> {
         // );
 
         read_chunk_nbt(
-            i,
             &mut cursor,
             file_header_size + (chunk_position * 4096) as u64,
         )
@@ -107,32 +55,45 @@ pub fn read_region_file(file_path: String) -> Vec<Chunk> {
     vec![]
 }
 
-fn read_chunk_nbt(i: usize, cursor: &mut Cursor<Vec<u8>>, pointer: u64) {
+fn read_chunk_nbt(cursor: &mut Cursor<Vec<u8>>, pointer: u64) {
     cursor.set_position(pointer);
 
     let chunk_nbt = match io::read_nbt(cursor, Flavor::ZlibCompressed) {
         //nbt data for the current chunk
         Ok(chunk_nbt) => chunk_nbt.0,
         Err(e) => {
-            println!("Error reading NBT: {}, inx {}", e, i);
+            println!("Error reading NBT: {}", e);
             return;
         }
     };
 
     let sections: &NbtList = chunk_nbt.get::<_, _>("sections").unwrap();
 
+    let mut chunk_sections: Vec<String> = Vec::new();
+
     for i in 0..sections.len() {
         let section = sections.get::<&NbtCompound>(i).unwrap();
 
-        println!("Section: {:?}", section);
+        //println!("Section: {:?}", section);
         // let section = sections.get::<&NbtCompound>(i).unwrap();
-        // let block_states = match section.get::<_, &NbtCompound>("block_states") {
-        //     Ok(block_data) => block_data,
-        //     Err(_) => {
-        //         //println!("Section {} is empty", section);
-        //         continue;
-        //     }
-        // };
+        let biomes = match section.get::<_, &NbtCompound>("biomes") {
+            Ok(block_data) => PalettedContainer::from_nbt(block_data, 4096),
+            Err(_) => {
+                println!("An error occurred while reading biomes");
+                continue;
+            }
+        };
+        let block_states = match section.get::<_, &NbtCompound>("block_states") {
+            Ok(block_data) => PalettedContainer::from_nbt(block_data, 64),
+            Err(_) => {
+                println!("An error occurred while reading block states");
+                continue;
+            }
+        };
+
+        println!("Block states: {:?}", block_states);
+        println!("Biomes: {:?}", biomes);
+
         // let block_data = match block_states.get::<_, &[i64]>("data") {
         //     Ok(block_data) => block_data,
         //     Err(_) => {
@@ -157,14 +118,70 @@ fn read_chunk_nbt(i: usize, cursor: &mut Cursor<Vec<u8>>, pointer: u64) {
 }
 
 #[derive(Debug)]
+struct PalettedContainer<'a> {
+    pub palette: &'a NbtList,
+    pub data: Option<BitArray<'a>>,
+    pub size: usize,
+}
+
+impl PalettedContainer<'_> {
+    fn from_nbt<'a>(nbt: &NbtCompound, size: usize) -> PalettedContainer {
+        let data = match nbt.get::<_, &[i64]>("data") {
+            Ok(data) => Some(BitArray::new(data, size.clone())),
+            Err(_) => None,
+        };
+        let palette = nbt.get::<_, &NbtList>("palette").unwrap();
+
+        PalettedContainer::new(data, palette, size)
+    }
+    fn new<'a>(
+        data: Option<BitArray<'a>>,
+        palette: &'a NbtList,
+        size: usize,
+    ) -> PalettedContainer<'a> {
+        PalettedContainer {
+            palette,
+            data,
+            size,
+        }
+    }
+    // fn get_3d(&self, x: usize, y: usize, z: usize, width: usize) -> usize {
+    //     const SECTION_WIDTH: usize = 16;
+    //     const SECTION_HEIGHT: usize = 16;
+    //     let block_number: usize = ((((y * SECTION_HEIGHT) + z) * SECTION_WIDTH) + x) as usize;
+    //     self.get(block_number)
+    // }
+
+    // BROKEN
+    fn get(&self, index: usize) -> usize {
+        match self.data {
+            Some(ref data) => {
+                // let bits_per_block = data.bits_per_value;
+                // let block_id = data.data.get(index).unwrap();
+                // let block_state = self
+                //     .palette
+                //     .get::<&NbtCompound>(block_id.clone() as usize)
+                //     .unwrap();
+                // let block_name = block_state.get::<_, &str>("Name").unwrap();
+                // let block_name = block_name.replace("minecraft:", "");
+                // println!("Block name: {}", block_name);
+                // block_name.parse::<usize>().unwrap()
+                0
+            }
+            None => 0,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct BitArray<'a> {
-    data: &'a [i64],
-    bits_per_value: usize,
+    pub data: &'a [i64],
+    pub bits_per_value: usize,
     individual_value_mask: i64,
 }
 
 impl BitArray<'_> {
-    fn new<'a>(data: &'a [i64], size: usize) -> BitArray {
+    fn new<'a>(data: &'a [i64], size: usize) -> BitArray<'a> {
         let bits_per_value = ((data.len() * 64) + size - 1) / size;
         let individual_value_mask = (1 << bits_per_value) - 1;
         BitArray {
@@ -172,18 +189,5 @@ impl BitArray<'_> {
             bits_per_value,
             individual_value_mask,
         }
-    }
-    fn get_3d(&self, x: usize, y: usize, z: usize) -> usize {
-        const SECTION_WIDTH: usize = 16;
-        const SECTION_HEIGHT: usize = 16;
-        let block_number: usize = ((((y * SECTION_HEIGHT) + z) * SECTION_WIDTH) + x) as usize;
-        self.get(block_number)
-    }
-    fn get(&self, index: usize) -> usize {
-        let values_per_long = 64 / self.bits_per_value;
-        let long_index = index / values_per_long;
-        let long_value = self.data[long_index];
-        let value_index = index % values_per_long;
-        ((long_value >> (value_index * self.bits_per_value)) & self.individual_value_mask) as usize
     }
 }
