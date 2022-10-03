43149083a4b533214d937c41fbf32382ea160415
Numericly <107078148+numericly@users.noreply.github.com>
2022-10-02 22:31:15 -0400

    some stuff

--- world/src/blocks.rs
+++ world/src/blocks.rs
@@ -120,13 +120,34 @@ macro_rules! blocks {
             use std::str::FromStr;
             use super::BlockProperties;
 
+            #[derive(Deserialize, Debug)]
+            pub struct ConstrainedInt<const MIN: u32, const MAX: u32> (pub u32);
+
+            impl<const MIN: u32, const MAX: u32> FromStr for ConstrainedInt<MIN, MAX> {
+                type Err = String;
+
+                fn from_str(s: &str) -> Result<Self, Self::Err> {
+                    let val = s.parse::<u32>().map_err(|e| e.to_string())?;
+                    if val > MAX {
+                        return Err(format!("Value {} is greater than the maximum value {}", val, MAX));
+                    }
+                    if val < MIN {
+                        return Err(format!("Value {} is less than the minimum value {}", val, MIN));
+                    }
+
+                    Ok(Self(val))
+                }
+            }
+
+            impl<const MIN: u32, const MAX: u32> ConstrainedInt<MIN, MAX> {
+            }
+
             enum_property! { Axis { X => "x", Y => "y", Z => "z" } }
             enum_property! { HorizontalAxis { X => "x", Z => "z" } }
 
             enum_property! { Facing { North => "north", East => "east", South => "south", West => "west", Up => "up", Down => "down" } }
             enum_property! { HopperFacing { North => "north", East => "east", South => "south", West => "west", Down => "down" } }
             enum_property! { HorizontalFacing { North => "north", East => "east", South => "south", West => "west" } }
-
             enum_property! { Orientation {
                 DownEast => "down_east",
                 DownNorth => "down_north",
@@ -147,7 +168,63 @@ macro_rules! blocks {
             enum_property! { RedstoneSide { Up => "up", None => "none", Side => "side" } }
             enum_property! { DoubleBlockHalf { Lower => "lower", Upper => "upper" } }
             enum_property! { Half { Top => "top", Bottom => "bottom" } }
-
+            enum_property! { RailShape {
+                NorthSouth => "north_south",
+                EastWest => "east_west",
+                AscendingEast => "ascending_east",
+                AscendingWest => "ascending_west",
+                AscendingNorth => "ascending_north",
+                AscendingSouth => "ascending_south",
+                SouthEast => "south_east",
+                SouthWest => "south_west",
+                NorthWest => "north_west",
+                NorthEast => "north_east"
+            }}
+            enum_property! { StraightRailShape {
+                NorthSouth => "north_south",
+                EastWest => "east_west",
+                AscendingEast => "ascending_east",
+                AscendingWest => "ascending_west",
+                AscendingNorth => "ascending_north",
+                AscendingSouth => "ascending_south"
+            }}
+            enum_property! { BedPart { Head => "head", Foot => "foot" } }
+            enum_property! { ChestType { Single => "single", Left => "left", Right => "right" } }
+            enum_property! { ComparatorMode { Compare => "compare", Subtract => "subtract" } }
+            enum_property! { DoorHinge { Left => "left", Right => "right" } }
+            enum_property! { NoteBlockInstrument {
+                Harp => "harp",
+                DoubleBass => "basedrum",
+                Snare => "snare",
+                Hat => "hat",
+                Bells => "bass",
+                Flute => "flute",
+                Bell => "bell",
+                Guitar => "guitar",
+                Chime => "chime",
+                Xylophone => "xylophone",
+                IronXylophone => "iron_xylophone",
+                CowBell => "cow_bell",
+                Didgeridoo => "didgeridoo",
+                Bit => "bit",
+                Banjo => "banjo",
+                Pling => "pling"
+            }}
+            enum_property! { PistonType { Normal => "normal", Sticky => "sticky" } }
+            enum_property! { SlabType { Top => "top", Bottom => "bottom", Double => "double" } }
+            enum_property! { StairsShape {
+                Straight => "straight",
+                InnerLeft => "inner_left",
+                InnerRight => "inner_right",
+                OuterLeft => "outer_left",
+                OuterRight => "outer_right"
+            }}
+            enum_property! { StructureBlockMode { Save => "save", Load => "load", Corner => "corner", Data => "data" } }
+            enum_property! { BambooLeaves { None => "none", Small => "small", Large => "large" } }
+            enum_property! { Tilt { None => "none", Unstable => "unstable", Partial => "partial", Full => "full" } }
+            enum_property! { VerticalDirection { Up => "up", Down => "down" } }
+            enum_property! { DripStoneThickness { TipMerge => "tip_merge", Tip => "tip", Frustum => "frustum", Middle => "middle", Base => "base" } }
+            enum_property! { SculkSensorPhase { Inactive => "inactive", Active => "active", CoolDown => "cooldown" } }
 
             block_props!(
                 attached => bool,
@@ -196,10 +273,36 @@ macro_rules! blocks {
                 attach_face => AttachFace,
                 attachment => BellAttach,
                 half,
-                shape
-
-
+                shape,
+                age,
+                bites => ConstrainedInt::<0, 6>,
+                candles => ConstrainedInt::<1, 4>,
+                delay => ConstrainedInt::<1, 4>,
+                distance,
+                eggs => ConstrainedInt::<1, 4>,
+                hatch => ConstrainedInt::<0, 2>,
+                layers => ConstrainedInt::<1, 8>,
+                level,
+                honey_level => ConstrainedInt::<0, 5>,
+                moisture => ConstrainedInt::<0, 7>,
+                note => ConstrainedInt::<0, 24>,
+                pickles => ConstrainedInt::<1, 4>,
+                power => ConstrainedInt::<0, 15>,
+                stage => ConstrainedInt::<0, 1>,
+                charges => ConstrainedInt::<0, 4>,
+                rotation => ConstrainedInt::<0, 15>,
+                part => BedPart,
+                chest_type => ChestType,
+                mode,
+                hinge => DoorHinge,
+                instrument => NoteBlockInstrument,
+                leaves => BambooLeaves,
+                tilt => Tilt,
+                vertical_direction => VerticalDirection,
+                thickness => DripStoneThickness,
+                phase => SculkSensorPhase
             );
+
 
             $(
                 #[allow(non_upper_case_globals)]
@@ -254,11 +357,17 @@ blocks!(
             can_occlude: false,
             is_air: true,
             ..BlockProperties::new(crate::materials::Materials::AIR)
+        }
+    },
+    GrassBlock, "minecraft:grass_block" => {
+        BlockProperties {
+            has_collision: true,
+            can_occlude: true,
+            is_air: false,
+            ..BlockProperties::new(crate::materials::Materials::GRASS)
         },
-        AirState {
-            axis, Axis: Axis::Y
+        GrassBlockState {
+            snowy: false
         }
     }
 );
-
-fn e() {}
