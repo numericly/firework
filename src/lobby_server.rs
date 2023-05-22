8e58062868334addfa78e9cd7b410d0a90aba6c6
numericly <107078148+numericly@users.noreply.github.com>
2023-05-22 08:09:48 -0400

    Added practice menu

--- src/lobby_server.rs
+++ src/lobby_server.rs
@@ -250,11 +250,11 @@ impl PlayerHandler<LobbyServerHandler, MiniGameProxy> for LobbyPlayerHandler {
                     .display_gui(client.server.handler.game_menu.clone())
                     .await;
             }
-            // Item::Stick => {
-            //     client
-            //         .display_gui(client.server.handler.practice_menu.clone())
-            //         .await;
-            // }
+            Item::Stick => {
+                client
+                    .display_gui(client.server.handler.practice_menu.clone())
+                    .await;
+            }
             Item::RedstoneBlock => {
                 leave_queue(client).await;
             }
@@ -359,11 +359,7 @@ impl LobbyPlayerHandler {
 pub struct GameMenu {
     pub items: Vec<ItemStack>,
     pub channel: broadcast::Sender<GUIEvent>,
-}
-
-pub struct PracticeMenu {
-    pub items: Vec<ItemStack>,
-    pub channel: broadcast::Sender<GUIEvent>,
+    pub r#type: MenuType,
 }
 
 #[async_trait]
@@ -433,16 +429,39 @@ impl GuiScreen<LobbyServerHandler, MiniGameProxy> for GameMenu {
             }
         }
 
-        match slot {
-            3 => {
-                queue(client, &client.proxy, MiniGame::Glide).await;
-                client.close_gui();
-            }
-            5 => {
-                queue(client, &client.proxy, MiniGame::Battle).await;
-                client.close_gui();
-            }
-            _ => {}
+        match self.r#type {
+            MenuType::Play => match slot {
+                3 => {
+                    queue(client, &client.proxy, MiniGame::Glide).await;
+                    client.close_gui();
+                }
+                5 => {
+                    queue(client, &client.proxy, MiniGame::Battle).await;
+                    client.close_gui();
+                }
+                _ => {}
+            },
+            MenuType::Practice => match slot {
+                3 => {
+                    let game_id = client
+                        .proxy
+                        .glide_queue
+                        .lock()
+                        .await
+                        .create_server(client.proxy.clone());
+                    client.transfer(TransferData::Glide { game_id });
+                }
+                5 => {
+                    let game_id = client
+                        .proxy
+                        .battle_queue
+                        .lock()
+                        .await
+                        .create_server(client.proxy.clone());
+                    client.transfer(TransferData::Battle { game_id })
+                }
+                _ => {}
+            },
         }
 
         Ok(())
@@ -450,9 +469,10 @@ impl GuiScreen<LobbyServerHandler, MiniGameProxy> for GameMenu {
 }
 
 impl GameMenu {
-    pub fn new() -> Self {
+    pub fn play() -> Self {
         let (sender, _) = broadcast::channel(1000);
         Self {
+            r#type: MenuType::Play,
             channel: sender,
             items: vec![
                 None,
@@ -505,134 +525,10 @@ impl GameMenu {
             ]
         }
     }
-    async fn correct_item(
-        &self,
-        client: &Client<LobbyServerHandler, MiniGameProxy>,
-        slot: usize,
-    ) -> ItemStack {
-        if slot < WindowType::Generic9x1.len() {
-            self.items[slot].clone()
-        } else if slot >= WindowType::Generic9x1.len() && slot < WindowType::Generic9x1.len() + 9 {
-            client
-                .player
-                .read()
-                .await
-                .inventory
-                .get_main_slot_from_container(slot - (WindowType::Generic9x1.len()))
-                .clone()
-        } else if slot >= WindowType::Generic9x1.len() + 27
-            && slot < WindowType::Generic9x1.len() + 27 + 9
-        {
-            client
-                .player
-                .read()
-                .await
-                .inventory
-                .get_hotbar_slot_from_container(slot - (WindowType::Generic9x1.len() + 27))
-                .clone()
-        } else {
-            None
-        }
-    }
-}
-
-#[async_trait]
-impl GuiScreen<LobbyServerHandler, MiniGameProxy> for PracticeMenu {
-    async fn init(&self, _client: &Client<LobbyServerHandler, MiniGameProxy>) -> GUIInit {
-        GUIInit {
-            title: r#"{"text":"      Practice Minigames","bold":true}"#.to_string(),
-            window_type: WindowType::Generic9x1,
-            items: self.items.clone(),
-            receiver: self.channel.subscribe(),
-        }
-    }
-    async fn handle_click(
-        &self,
-        slot: ClickContainer,
-        client: &Client<LobbyServerHandler, MiniGameProxy>,
-    ) -> Result<(), ConnectionError> {
-        let ClickContainer {
-            window_id,
-            state_id,
-            slot,
-            button: _button,
-            mode,
-            slots,
-        } = slot;
-
-        match mode {
-            InventoryOperationMode::Click => {
-                if slot >= 0 {
-                    client
-                        .send_packet(SetContainerSlot {
-                            window_id: -1,
-                            state_id: VarInt(state_id.0 + 1),
-                            slot: -1,
-                            item: None,
-                        })
-                        .await?;
-                    let item = self.correct_item(client, slot as usize).await;
-
-                    client
-                        .send_packet(SetContainerSlot {
-                            window_id: window_id as i8,
-                            state_id: VarInt(state_id.0 + 2),
-                            slot,
-                            item,
-                        })
-                        .await?;
-                }
-            }
-            _ => {}
-        }
-
-        for (i, updated_slot) in slots.iter().enumerate() {
-            if updated_slot.slot_number >= 0 {
-                let item = self
-                    .correct_item(client, updated_slot.slot_number as usize)
-                    .await;
-
-                client
-                    .send_packet(SetContainerSlot {
-                        window_id: window_id as i8,
-                        state_id: VarInt(state_id.0 + i as i32 + 1),
-                        slot: updated_slot.slot_number,
-                        item,
-                    })
-                    .await?;
-            }
-        }
-
-        match slot {
-            3 => {
-                let game_id = client
-                    .proxy
-                    .glide_queue
-                    .lock()
-                    .await
-                    .create_server(client.proxy.clone());
-                client.transfer(TransferData::Glide { game_id });
-            }
-            5 => {
-                let game_id = client
-                    .proxy
-                    .battle_queue
-                    .lock()
-                    .await
-                    .create_server(client.proxy.clone());
-                client.transfer(TransferData::Battle { game_id })
-            }
-            _ => {}
-        }
-
-        Ok(())
-    }
-}
-
-impl PracticeMenu {
-    pub fn new() -> Self {
+    pub fn practice() -> Self {
         let (sender, _) = broadcast::channel(1000);
         Self {
+            r#type: MenuType::Practice,
             channel: sender,
             items: vec![
                 None,
@@ -719,7 +615,13 @@ impl PracticeMenu {
 pub struct LobbyServerHandler {
     commands: CommandTree<Self, MiniGameProxy>,
     game_menu: Arc<GameMenu>,
-    practice_menu: Arc<PracticeMenu>,
+    practice_menu: Arc<GameMenu>,
+}
+
+#[derive(Debug, Clone)]
+pub enum MenuType {
+    Play,
+    Practice,
 }
 
 #[async_trait]
@@ -728,8 +630,8 @@ impl ServerHandler<MiniGameProxy> for LobbyServerHandler {
     type PlayerHandler = LobbyPlayerHandler;
     fn new() -> Self {
         Self {
-            game_menu: Arc::new(GameMenu::new()),
-            practice_menu: Arc::new(PracticeMenu::new()),
+            game_menu: Arc::new(GameMenu::play()),
+            practice_menu: Arc::new(GameMenu::practice()),
             commands: CommandTree::new()
                 .register_command(
                     Command::new("practice", "practice a minigame in a private lobby").add_node(
